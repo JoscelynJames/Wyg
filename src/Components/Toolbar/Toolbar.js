ea9896875e7d767db4d7727673e7aea0153c4f73
Joscelyn James <josce.james7@hotmail.com>
2019-12-21 08:20:46 -0700

    Added toolbar component

---
+++ src/Components/Toolbar/Toolbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Toolbar.scss'
+
+function Toolbar() {
+  return <div id="toolbar"></div>
+}
+
+export default Toolbar
--- src/Playground.js
+++ src/Playground.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Playground.scss'
-import Editor from './Components/Editor'
+import Editor from './Components/Editor/Editor'
+import Toolbar from './Components/Toolbar/Toolbar'
 
 function Playground() {
   return (
     <div className="Playground">
-      <Editor></Editor>
+      <Editor>
+        <Toolbar />
+      </Editor>
     </div>
   )
 }
