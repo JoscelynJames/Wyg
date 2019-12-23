4be4b18cd4beda9ff9b42dd924a1b6ee37c40811
Joscelyn James <josce.james7@hotmail.com>
2019-12-23 09:12:08 -0700

    tried to test more ColorPickerTool - would need to eject in order to test further

--- src/Components/Tools/ColorPicker/ColorPickerTool.js
+++ src/Components/Tools/ColorPicker/ColorPickerTool.js
@@ -2,21 +2,17 @@ import React from 'react'
 import { SketchPicker } from 'react-color'
 import './ColorPickerTool.scss'
 import withTool from '../WithTool/WithTool'
-
 class ColorPickerTool extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showColorPicker: false,
-      currentColor: '#50E3C2'
-    }
+  state = {
+    showColorPicker: false,
+    currentColor: '#50E3C2'
   }
 
   toggleColorPicker(newStatus) {
     this.setState({ showColorPicker: newStatus })
   }
 
-  async handleChange(color, event) {
+  handleChange(color, event) {
     // we want to make sure that the text stays highlighted on click
     event.preventDefault()
     // this will change just the color of the text
--- src/Components/Tools/ColorPicker/ColorPickerTool.spec.js
+++ src/Components/Tools/ColorPicker/ColorPickerTool.spec.js
@@ -1,17 +1,48 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import ColorPickerTool from './ColorPickerTool'
+import { JSDOM } from 'jsdom'
+
+const dom = new JSDOM()
+global.document = dom.window.document
+global.window = dom.window
 
 function setup() {
   const props = {}
+  // for component wrapped in a higher order component follow this convention
+  // Github issue - https://github.com/airbnb/enzyme/issues/539
   const component = shallow(<ColorPickerTool {...props} />)
+    .first()
+    .shallow()
 
   return { component, props }
 }
 
 describe('ColorPickerTool testing suite', () => {
+  const dom = new JSDOM()
+  global.document = dom.window.document
+  global.window = dom.window
+
   it('renders ColorPickerTool', () => {
     const { component } = setup()
     expect(component).toBeDefined()
   })
+
+  it('#toggleColorPicker', () => {
+    const { component } = setup()
+    expect(component.instance().state.showColorPicker).toEqual(false)
+
+    component.instance().toggleColorPicker(true)
+    expect(component.instance().state.showColorPicker).toEqual(true)
+  })
+
+  // we need to mock the DOM in order to test this function - this will require an eject
+  // https://github.com/facebook/jest/issues/2297 - create react app does not support this
+  // it('#handleChange', () => {
+  //   const { component } = setup()
+  //   expect(component.instance().state.currentColor).toEqual('#50E3C2')
+  //   const event = { preventDefault: () => {} } // mock event
+  //   component.instance().handleChange({ hex: '#EEEEEE' }, event)
+  //   expect(component.instance().state.showColorPicker).toEqual('#EEEEEE')
+  // })
 })
