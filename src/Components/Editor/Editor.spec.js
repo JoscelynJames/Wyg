e16e520e63d536afd1e362c4237443b8fb57b453
Joscelyn James <josce.james7@hotmail.com>
2019-12-23 18:32:00 -0700

    updated test

--- src/Components/Editor/Editor.spec.js
+++ src/Components/Editor/Editor.spec.js
@@ -14,4 +14,30 @@ describe('Editor testing suite', () => {
     const { component } = setup()
     expect(component).toBeDefined()
   })
+
+  it('#activateEditor', () => {
+    const { component } = setup()
+    expect(component.instance().state.active).toEqual(false)
+
+    component.instance().activateEditor()
+    expect(component.instance().state.active).toEqual(true)
+  })
+
+  it('#setEditor', () => {
+    const { component } = setup()
+    expect(component.instance().state.editor).toEqual(undefined)
+
+    component.instance().setEditor({ woo: 'im an editor' })
+    expect(component.instance().state.editor).toEqual({ woo: 'im an editor' })
+  })
+
+  it('#selectTool', () => {
+    const { component } = setup()
+    expect(component.instance().state.selectedTool).toEqual('text')
+
+    component
+      .instance()
+      .selectTool('cool new tool', { preventDefault: () => {} })
+    expect(component.instance().state.selectedTool).toEqual('cool new tool')
+  })
 })
