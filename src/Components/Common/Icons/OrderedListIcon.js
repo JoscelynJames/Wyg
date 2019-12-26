84fc0bf0ebaf8be4ee94bff4a7e462142e36fbb4
Joscelyn James <josce.james7@hotmail.com>
2019-12-26 14:35:03 -0700

    Partial list tool icon (will add svg when online)

---
+++ src/Components/Common/Icons/OrderedListIcon.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function OrderedListIcon({
+  handleMouseDown,
+  style = { margin: '10px auto' },
+  fill = '#000',
+  width = '24',
+  className = '',
+  height = '24',
+  viewBox = '0 0 24 24'
+}) {
+  return (
+    // <svg
+    //   width={width}
+    //   style={style}
+    //   height={height}
+    //   viewBox={viewBox}
+    //   className={className}
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   xmlnsXlink="http://www.w3.org/1999/xlink"
+    // >
+
+    // </svg>
+    <p>ol</p>
+  )
+}
+
+export default OrderedListIcon
